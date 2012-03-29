eb376f93a4e2d0c5166cebb64dd3130ceaa50440
Derek (momo-castella) <derekathomas@gmail.com>
2012-03-29 15:19:42 +0900

    New history keys for zsh

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -3,8 +3,16 @@
 bindkey -v
 
 # history search
-bindkey "^p" history-beginning-search-backward
-bindkey "^n" history-beginning-search-forward
+autoload -z history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^n" history-beginning-search-forward-end
+
+# tab completion history menu (vicmd)
+autoload -z history-beginning-search-menu
+zle -N history-beginning-search-menu-space-end history-beginning-search-menu
+bindkey -M vicmd "\t"  history-beginning-search-menu-space-end
 
 # clear the screen
 bindkey "^L" clear-screen
