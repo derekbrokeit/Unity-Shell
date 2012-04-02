4d0efb9941e745898e0018f2368f0ef47605a90f
Derek (harp) <derekathomas@gmail.com>
2012-04-02 15:41:41 +0900

    Move GPG agent startup to local computer only section in zsh

--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -52,6 +52,39 @@ if [[ "$COMP_TYPE" == "local" ]] ; then
   # keep log files together
   export LOGS_DIR="$DROPBOX/serverLogs"
 
+  # --- GPG agent initialization
+  # # setup gpg (this is the old way)
+  if [[ "x$(which gpg2)" != "x" ]] ; then
+  export GPG_TTY=$(tty) 
+  eval $(gpg-agent --daemon) 
+  fi
+
+  # the following is a better way from Oh-my-zsh
+  # local GPG_ENV=$HOME/.gnupg/gpg-agent.env
+
+  # function start_agent {
+  #   /usr/bin/env gpg-agent --daemon --write-env-file ${GPG_ENV} > /dev/null
+  #   chmod 600 ${GPG_ENV}
+  #   . ${GPG_ENV} > /dev/null
+  # }
+
+  # # Source GPG agent settings, if applicable
+  # if [ -f "${GPG_ENV}" ]; then
+  #   . ${GPG_ENV} > /dev/null
+  #   # ps -ef | grep ${SSH_AGENT_PID} | grep gpg-agent > /dev/null || {
+  #   ps -ef  | grep gpg-agent > /dev/null || {
+  #     start_agent;
+  #   }
+  # else
+  #   start_agent;
+  # fi
+
+  # export GPG_AGENT_INFO
+  # export SSH_AUTH_SOCK
+  # export SSH_AGENT_PID
+
+  # export GPG_TTY=$(tty)
+
   ### Remote Server variables {{{1
 elif [[ "$COMP_TYPE" == "central" ]] || [[ "$COMP_TYPE" == "remote" ]] ; then
 
@@ -119,35 +152,3 @@ echo $F90_FLAGS > $HOME/.f90flags
 export F90=$F90_COMP
 export F77=$F90
 
-# --- GPG agent initialization
-# # setup gpg (this is the old way)
-if [[ "x$(which gpg2)" != "x" ]] ; then
-  export GPG_TTY=$(tty) 
-  eval $(gpg-agent --daemon) 
-fi
-
-# the following is a better way from Oh-my-zsh
-# local GPG_ENV=$HOME/.gnupg/gpg-agent.env
-
-# function start_agent {
-#   /usr/bin/env gpg-agent --daemon --write-env-file ${GPG_ENV} > /dev/null
-#   chmod 600 ${GPG_ENV}
-#   . ${GPG_ENV} > /dev/null
-# }
-
-# # Source GPG agent settings, if applicable
-# if [ -f "${GPG_ENV}" ]; then
-#   . ${GPG_ENV} > /dev/null
-#   # ps -ef | grep ${SSH_AGENT_PID} | grep gpg-agent > /dev/null || {
-#   ps -ef  | grep gpg-agent > /dev/null || {
-#     start_agent;
-#   }
-# else
-#   start_agent;
-# fi
-
-# export GPG_AGENT_INFO
-# export SSH_AUTH_SOCK
-# export SSH_AGENT_PID
-
-# export GPG_TTY=$(tty)
