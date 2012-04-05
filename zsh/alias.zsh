11e25ff145642923b6653e80f128bab831c2f3b4
Derek (momo-castella) <derekathomas@gmail.com>
2012-04-05 16:25:45 +0900

    load the actual $SHELL, not necessarily the shell that the OS chooses ; also may change per terminal ... if that's what you like

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -88,7 +88,7 @@ alias pizza="python ~/dev/py-pizza/src/pizza.py"
 
 # remap mutt to tmuxified mutt
 # currently causing strange behavior, fixing
-alias mutt="tmux neww -n mutt -t 7 \"zsh -c 'source $HOME/.zshrc ; mutt'\" "
+alias mutt="tmux neww -n mutt -t 7 \"$SHELL -c 'source $HOME/.$(basename $SHELL)rc ; mutt'\" "
 
 # syntax highlighter cat
 alias pcat="pygmentize -g"
