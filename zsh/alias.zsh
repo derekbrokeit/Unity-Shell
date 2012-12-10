98329eb8e6f5179b4cbca06ba36919a47efe49ab
Derek Thomas <derekathomas@gmail.com>
2012-12-10 17:13:24 +0900

    Add zsh aliases to remove library errors in system commands/functions

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -46,3 +46,30 @@ compdef _git gm=git-merge
 # autocomplete vim
 compdef tmvim=vim
 compdef vi=vim
+
+# get rid of dyld libraries for system functions
+alias ps="env -u DYLD_INSERT_LIBRARIES ps"
+alias rcp="env -u DYLD_INSERT_LIBRARIES rcp"
+alias at="env -u DYLD_INSERT_LIBRARIES at"
+alias atq="env -u DYLD_INSERT_LIBRARIES atq"
+alias atrm="env -u DYLD_INSERT_LIBRARIES atrm"
+alias batch="env -u DYLD_INSERT_LIBRARIES batch"
+alias crontab="env -u DYLD_INSERT_LIBRARIES crontab"
+alias ipcs="env -u DYLD_INSERT_LIBRARIES ipcs"
+alias lockfile="env -u DYLD_INSERT_LIBRARIES lockfile"
+alias login="env -u DYLD_INSERT_LIBRARIES login"
+alias newgrp="env -u DYLD_INSERT_LIBRARIES newgrp"
+alias procmail="env -u DYLD_INSERT_LIBRARIES procmail"
+alias quota="env -u DYLD_INSERT_LIBRARIES quota"
+alias rlogin="env -u DYLD_INSERT_LIBRARIES rlogin"
+alias rsh="env -u DYLD_INSERT_LIBRARIES rsh"
+alias su="env -u DYLD_INSERT_LIBRARIES su"
+alias sudo="env -u DYLD_INSERT_LIBRARIES sudo"
+alias top="env -u DYLD_INSERT_LIBRARIES top"
+alias wall="env -u DYLD_INSERT_LIBRARIES wall"
+alias write="env -u DYLD_INSERT_LIBRARIES write"
+alias postdrop="env -u DYLD_INSERT_LIBRARIES postdrop"
+alias postqueue="env -u DYLD_INSERT_LIBRARIES postqueue"
+alias scselect="env -u DYLD_INSERT_LIBRARIES scselect"
+alias traceroute="env -u DYLD_INSERT_LIBRARIES traceroute"
+alias traceroute6="env -u DYLD_INSERT_LIBRARIES traceroute6"
