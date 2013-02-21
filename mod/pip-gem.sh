36d70afd5eb32f04c5debd918f2ccbfbb29d72e5
Derek Thomas <derekathomas@gmail.com>
2013-02-22 02:10:03 +0900

    Move some pip installations to unused array because alternate inst. method is used

--- mod/pip-gem.sh
+++ mod/pip-gem.sh
@@ -6,8 +6,9 @@ if [[ -f $HOME/.colors ]] ; then
 fi
 
 gems=( "maid" "terminal-notifier" "vmail" "compass" "pdfbeads" )
-pips=( "virtualenv" "virtualenvwrapper" "fabulous" "docutils" "numpy" "scipy" "ipython" "pygments" "pyzmq" "git+https://github.com/matplotlib/matplotlib.git#egg=matplotlib-dev" "git+https://github.com/hyde/typogrify.git#egg=typogrify-hyde" "cython" "lxml" )
+pips=( "virtualenv" "virtualenvwrapper" "fabulous" "docutils" "ipython" "pygments" "pyzmq" "git+https://github.com/hyde/typogrify.git#egg=typogrify-hyde" "cython" "lxml" "nose" )
 pip_editable=( "git://github.com/hyde/hyde.git#egg=hyde" "git+https://github.com/Lokaltog/powerline.git@develop#egg=powerline" "$HOME/dev/crystalpy" "$HOME/dev/analyzer" )
+reference=( "numpy" "scipy" "git+https://github.com/matplotlib/matplotlib.git#egg=matplotlib-dev" )
 
 
 case $1 in
