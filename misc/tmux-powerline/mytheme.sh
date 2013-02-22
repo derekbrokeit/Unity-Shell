b9053909683928b563f7873d80659c4855dd573f
Derek Thomas <derekathomas@gmail.com>
2013-02-22 15:02:31 +0900

    Rearrange tmux-powerline theme

--- misc/tmux-powerline/mytheme.sh
+++ misc/tmux-powerline/mytheme.sh
@@ -44,10 +44,10 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
         "mailcount 9 255" \
         #"now_playing 234 37" \
 		#"cpu 240 136" \
-        "load 237 167" \
-        #"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		"battery 144 127" \
 		"weather 37 255" \
+        "load 238 167" \
+        #"tmux_mem_cpu_load 234 136" \
 		#"xkb_layout 125 117" \
 		"date_day 235 12" \
 		"date 235 12 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
