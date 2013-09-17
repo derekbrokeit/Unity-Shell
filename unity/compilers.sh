087a126738bc71151c00ff2706921abbf5268f01
Derek Thomas <derekathomas@gmail.com>
2013-09-17 16:52:22 +0900

    compilers: only set variables if special compilers exist, otherwise do not export

--- unity/compilers.sh
+++ unity/compilers.sh
@@ -1,50 +1,42 @@
 # setup prefered compilers on linux distros
 if os_is_linux ; then
-    local host="SSE4.2"
-    local default_opt="-O2 -m$host"
+    host="SSE4.2"
+    default_opt="-O2"
 
     # Fortran
     # ifort > gfortran
     if is_avail ifort ; then
-        FC=ifort
-        F77=ifort
-        FCFLAGS="-O3 -x$host -ipo"
-    else
-        FC=gfortran
-        F77=gfortran
-        FCFLAGS=$default_opt
+        export FC=ifort
+        export F77=ifort
+        export FCFLAGS="-O3 -x$host -ipo"
     fi
-    export FC
-    export F77
-    export FCFLAGS
 
     # c-compilers
     # icc > clang > gcc
     if is_avail icc ; then
-        CC=icc
-        CFLAGS="-O3 -x$host -ipo"
+        export CC=icc
+        export CFLAGS="-O3 -x$host -ipo"
     elif is_avail clang ; then
-        CC=clang
-        CFLAGS=$default_opt
-    else
-        CC=gcc
-        CFLAGS=$default_opt
+        export CC=clang
+        export CFLAGS=$default_opt
+    elif is_avail gcc-4.9 ; then
+        export CC=gcc-4.9
+        export CFLAGS=$default_opt
     fi
-    export CC
-    export CFLAGS
 
     # c++-compilers
     # icpc > clang++ > g++
     if is_avail icpc ; then
-        CXX=icpc
-        CXXFLAGS="-O3 -x$host -ipo"
+        export CXX=icpc
+        export CXXFLAGS="-O3 -x$host -ipo"
     elif is_avail clang++ ; then
-        CXX=clang++
-        CXXFLAGS=$default_opt
-    else
-        CXX=g++
-        CXXFLAGS=$default_opt
+        export CXX=clang++
+        export CXXFLAGS=$default_opt
+    elif is_avail g++-4.9 ; then
+        export CXX=g++-4.9
+        export CXXFLAGS=$default_opt
     fi
-    export CXX
-    export CXXFLAGS
+
+    unset host
+    unset default_opt
 fi
