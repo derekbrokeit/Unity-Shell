e50c0be8408967bd2a94291009ca77971e5b69d9
Derek A. Thomas <derekathomas@gmail.com>
2019-05-23 16:11:28 -0700

    fix MAGENTA color escaping in zsh

--- zsh/color.zsh
+++ zsh/color.zsh
@@ -6,7 +6,7 @@
 
 autoload -U colors && colors
 # set some colors
-for COLOR in RED GREEN YELLOW WHITE BLACK CYAN BLUE; do
+for COLOR in RED GREEN YELLOW WHITE BLACK CYAN BLUE MAGENTA; do
   eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
   eval PR_${COLOR}_BRIGHT='%{$fg_bold[${(L)COLOR}]%}'
 done
